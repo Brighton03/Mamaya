ebe6d3297fa23f285049836e17af420f06678a53
Brighton Tan Liang <s10185160@connect.np.edu.sg>
2022-01-07 03:36:40 +0800

    first commit

---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mamaya e-BookStore</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/site.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <a href="index.php">
+                <img src="Images/mamayaebooks.jpg" alt="Logo"
+                    class="img-fluid" style="width: 100%;"/></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <?php include("navbar.php");?>
+            </div>     
+        </div>
+        <div class="row">
+            <div class="col-sm-12" style="padding:15px;">
